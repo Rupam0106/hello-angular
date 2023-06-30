af824a4b0c40a95cc754019fdac16db2b20ff391
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-06-30 19:00:15 +0530

    generate components using cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 @Component({
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'homes';
 }
